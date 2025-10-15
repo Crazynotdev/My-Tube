45447bb194ab89c11c421137dbbc3b70655fc351
ZynXx <techigwe@gmail.com>
2025-10-15 20:22:19 +0100

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="max-w-7xl mx-auto p-4 prose dark:prose-invert">
+      <h1>À Propos de MyTube</h1>
+      <p>MyTube est une plateforme créer par Crazy gratuite pour rechercher et télécharger du contenu multimédia depuis diverses sources. Nous priorisons la simplicité et la qualité.</p>
+      <AdBanner />
+    </div>
+  );
+}
